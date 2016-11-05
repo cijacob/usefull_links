98f9bd2f78ef839064d4e8cd61e7beac07201658
Cindy Jacob <jacobcindy@live.be>
2016-11-05 03:07:26 +0100

    redirect comment wip

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,11 +3,13 @@ class CommentsController < ApplicationController
 
   def create
     @link = Link.find(params[:link_id])
-    @comment = Comment.new(comment_params)
-    @comment.user = current_user  
+    @comment = @link.comments.new(comment_params)
+    @comment.user = current_user
+
+
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to links_path, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
@@ -16,26 +18,13 @@ class CommentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
-  def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # DELETE /comments/1
   # DELETE /comments/1.json
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to links_path, notice: 'Comment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
