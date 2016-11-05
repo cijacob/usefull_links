646a6636a7e27c268555678d3c349eb5b954d048
Cindy Jacob <jacobcindy@live.be>
2016-11-05 03:38:12 +0100

    add foreign key to links & relations in models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105023128) do
+ActiveRecord::Schema.define(version: 20161105023558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,8 +40,10 @@ ActiveRecord::Schema.define(version: 20161105023128) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.integer  "category_id"
   end
 
+  add_index "links", ["category_id"], name: "index_links_on_category_id", using: :btree
   add_index "links", ["user_id"], name: "index_links_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
